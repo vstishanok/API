d72dc33ab31e5a3719eef5a03420032a6637baa9
Vadim <148060081+vstishanok@users.noreply.github.com>
2023-10-17 18:07:14 +0300

    Update WeatherForecastController.cs

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebAPI.Controllers
     {
         private static readonly string[] Summaries =
             {
-                "Freezingdfgdfg"
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
             };
 
         [HttpGet]
